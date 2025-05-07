90f0ab53a17bffa3c20e592d81ddba849a507a05
jungwj0706 <woojin_jung@WJJungs-MacBook-Pro.local>
2025-05-07 21:35:46 +0900

    불명확한 변수명  수정

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.54266581731856&lon
         console.log(data);
         document.getElementById("temp").textContent = `온도: ${(data.main.temp-273.15).toFixed(1)}℃`;
         document.getElementById("humidity").textContent = `습도: ${data.main.humidity}%`;
-        document.getElementById("wind").textContent = `바람: ${data.wind.speed.toFixed(1)}m/s`;
-        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`;
-    })
+        document.getElementById("wind").textContent = `바람: ${data.wind.speed.toFixed(1)}m/s`;    })
     .catch(error => {
         console.error('오류가 발생했습니다.', error);
     });
